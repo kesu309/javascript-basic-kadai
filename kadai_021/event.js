03376052c29dbb9d247a6031358c3bbda75d6955
kesu309 <k.shiga309@gmail.com>
2024-09-28 23:04:53 +0900

    課題提出

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -4,5 +4,12 @@ const text = document.getElementById('text');
 
 // HTML要素がクリックされたときにイベント処理を実行する
 btn.addEventListener('click', () => {
-  text.textContent = 'ボタンをクリックしました';
+  
+  //textに対して文字列（ボタンをクリックしました）を出力
+  // 2秒（2000ミリ秒）の待ち時間を設定し、非同期処理を実行する
+  setTimeout(() => {
+    text.textContent = 'ボタンをクリックしました';
+  }, 2000);
+
 });
+
